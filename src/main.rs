2b4bc81b8d53d127e8e78e9b526bbca955ab6585
Eimh <41556634+Eimh@users.noreply.github.com>
2025-04-04 18:37:12 -0700

    init 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use bevy::{
+    log::{Level, LogPlugin},
+    prelude::*,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(LogPlugin {
+            level: Level::INFO,
+            ..Default::default()
+        }))
+        .run();
 }
