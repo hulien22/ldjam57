1a65b2aad72997a7683b5626055ff3f1fc375bec
hulien22 <hejulien22@gmail.com>
2025-04-05 01:28:14 -0700

    change paddle away from kinematic so we have more control over rotation and acceleration

--- src/paddle.rs
+++ src/paddle.rs
@@ -1,7 +1,7 @@
 use bevy::{math::VectorSpace, prelude::*};
 use bevy_rapier2d::prelude::{
-    ActiveCollisionTypes, ActiveEvents, Collider, CollisionEvent, GravityScale,
-    KinematicCharacterController, RigidBody,
+    ActiveCollisionTypes, ActiveEvents, Collider, CollisionEvent, ExternalImpulse, GravityScale,
+    KinematicCharacterController, LockedAxes, RigidBody, Velocity,
 };
 use leafwing_input_manager::{input_map, prelude::*};
 
@@ -42,39 +42,59 @@ struct Paddle;
 fn spawn_paddle(mut commands: Commands) {
     commands.spawn((
         Paddle,
-        Sprite::from_color(Color::srgb(1.0, 0.0, 0.0), Vec2 { x: 10.0, y: 3.0 }),
+        Sprite::from_color(Color::srgb(1.0, 0.0, 0.0), Vec2 { x: 30.0, y: 3.0 }),
         Transform::from_xyz(0.0, 30.0, 0.0),
-        Collider::cuboid(5.0, 1.5),
-        RigidBody::KinematicPositionBased,
-        KinematicCharacterController::default(),
+        Collider::cuboid(15.0, 1.5),
+        RigidBody::Dynamic,
+        // KinematicCharacterController::default(),
         GravityScale(0.0),
         ActiveCollisionTypes::all(),
         ActiveEvents::COLLISION_EVENTS,
         StateScoped(AppState::Game),
         Name::new("Paddle"),
         InputManagerBundle::with_map(PaddleAction::default_bindings()),
+        // LockedAxes::ROTATION_LOCKED_Z,
+        Velocity::default(),
     ));
 }
 
 fn move_paddle(
-    mut query: Query<
-        (
-            &ActionState<PaddleAction>,
-            &mut Transform,
-            &mut KinematicCharacterController,
-        ),
-        With<Paddle>,
-    >,
+    mut query: Query<(&ActionState<PaddleAction>, &mut Transform, &mut Velocity), With<Paddle>>,
+    time: Res<Time>,
 ) {
-    let (action_state, mut transform, mut controller) =
+    let (action_state, mut transform, mut vel) =
         query.get_single_mut().expect("Failed to get paddle entity");
 
+    // lerp to target velocity
+    let mut target_lin_vel: Vec2 = Vec2::ZERO;
+    let mut lin_damping = time.delta_secs() * 10.0;
     if action_state.axis_pair(&PaddleAction::Move) != Vec2::ZERO {
-        // println!(
-        //     "Moving paddle by {:?}",
-        //     action_state.axis_pair(&PaddleAction::Move)
-        // );
-        controller.translation = Some(action_state.axis_pair(&PaddleAction::Move) * 5.0);
+        // controller.translation = Some(action_state.axis_pair(&PaddleAction::Move) * 5.0);
+        target_lin_vel = action_state.axis_pair(&PaddleAction::Move) * 100.0;
+        lin_damping *= 2.0;
+    }
+    vel.linvel = vel.linvel.lerp(target_lin_vel, lin_damping);
+
+    // handle rotation
+    const MAX_ROTATION: f32 = f32::to_radians(5.0);
+    if transform.rotation.z.abs() > MAX_ROTATION {
+        // past the limit, stop rotating
+        vel.angvel = 0.0;
+        transform.rotation.z = transform.rotation.z.signum() * MAX_ROTATION;
+    } else {
+        let target_ang_vel: f32;
+        let ang_damping: f32;
+        const ANG_VEL: f32 = 1.0;
+        const MIN_VEL: f32 = 0.1;
+        if target_lin_vel.x.abs() > MIN_VEL {
+            target_ang_vel = -ANG_VEL * target_lin_vel.x.signum();
+            ang_damping = time.delta_secs() * 10.0;
+        } else {
+            // rotate back towards zero
+            target_ang_vel = -ANG_VEL * transform.rotation.z.signum();
+            ang_damping = time.delta_secs() * 20.0;
+        }
+        vel.angvel = FloatExt::lerp(vel.angvel, target_ang_vel, ang_damping);
     }
 
     if action_state.just_pressed(&PaddleAction::Fire) {
